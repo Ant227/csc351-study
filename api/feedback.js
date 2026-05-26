da9dccb461b83bb89cbbd7fd73e5ad18983bd934
Ant <antbonekyaw@gmail.com>
2026-05-26 13:57:38 +0700

    fix: use CommonJS syntax for Vercel serverless function

--- api/feedback.js
+++ api/feedback.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -39,4 +39,4 @@ export default async function handler(req, res) {
   } catch (error) {
     return res.status(500).json({ error: 'Internal server error', message: error.message });
   }
-}
+};
